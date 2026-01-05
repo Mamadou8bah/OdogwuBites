03568ed4337e4374a6c1d33cf69502589829a6b5
mamadou8bah <mbah18791@gmail.com>
2026-01-05 07:12:27 +0000

    Configured dashboard route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Homepage } from './homepage/homepage';
 import { MenuPage } from './menu-page/menu-page';
 import { AboutPage } from './about-page/about-page';
 import { LoginPage } from './login-page/login-page';
+import { Dashboard } from './dashboard/dashboard';
 import { Cart } from './cart/cart';
 export const routes: Routes = [
     {
@@ -24,5 +25,13 @@ export const routes: Routes = [
         path:'cart',
         component:Cart
 
+    },{
+        path:'dashboard',
+        component:Dashboard,
+        children:[
+            {
+                
+            }
+        ]
     }
 ];
