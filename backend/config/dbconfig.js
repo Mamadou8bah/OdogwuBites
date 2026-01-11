32a9ca4868e4251366f365feaebf57ae1aa2006e
mamadou8bah <mbah18791@gmail.com>
2026-01-11 12:58:12 +0000

    Fixed db url typo

--- backend/config/dbconfig.js
+++ backend/config/dbconfig.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 async function connectToDatabase() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Succesfully connected to a database');
     } catch (error) {
         console.error('Error connecting to DB', error);
